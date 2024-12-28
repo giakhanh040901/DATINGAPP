c1a7543c4954a9f2724621621501bbdf0b22d4bd
unknown <khanhdg@epic.local>
2024-12-28 22:33:23 +0700

    init

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using DATINGAPP.Data;
+using DATINGAPP.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace DATINGAPP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null)
+                return NotFound();
+            return user;
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using DATINGAPP.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DATINGAPP.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
