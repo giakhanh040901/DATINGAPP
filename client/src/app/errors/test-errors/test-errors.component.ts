02538b7cb07f42a6228da8e2adc729d0806cd43c
Duong Gia Khanh <khanhdg@meeyland.local>
2025-01-13 19:15:36 +0700

    End section 7

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -11,6 +11,7 @@ import { Component, inject } from '@angular/core';
 export class TestErrorsComponent {
   baseUrl = 'https://localhost:5001/api/';
   private http = inject(HttpClient);
+  validationErrors: string[] = [];
   
   get400Error() {
     this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
@@ -63,6 +64,7 @@ export class TestErrorsComponent {
       },
       error: (error) => {
         console.log(error);
+        this.validationErrors = error;
       },
     });
   }
